577a33dd21b8246416b20b6ece7ffb79a9b10896
Pavel Urbanovich <yrbik@mail.ru>
2022-11-22 22:09:39 +0300

    fix: finish task

--- src/index.js
+++ src/index.js
@@ -17,10 +17,9 @@ module.exports = function check(str, bracketsConfig) {
       
       if(str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === '3' || str[i] === '5' || str[i] === '1') {
         stack.push(str[i]);
-        
       } else if(str[i] === '|' || str[i] === '7' || str[i] === '8') {
         count++;
-        if (count % 2 === 0) {
+        if (stack[stack.length - 1] === map[str[i]]) {
           stack.pop();
         } else {
           stack.push(str[i]);
