6dd9f8b049c13f1f6be6c1f90b12db04ace4366a
Pavel Urbanovich <yrbik@mail.ru>
2022-11-22 21:11:54 +0300

    fix: add solution for 2 tests

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ module.exports = function check(str, bracketsConfig) {
     ']': '[',
     '}': '{',
     '|': '|',
-    '2': '2',
+    '2': '1',
     '4': '3',
     '6': '5',
     '7': '7',
@@ -15,19 +15,21 @@ module.exports = function check(str, bracketsConfig) {
   
   for (let i = 0; i < str.length; i++) {
       
-      if(str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === '3' || str[i] === '5') {
+      if(str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === '3' || str[i] === '5' || str[i] === '1') {
         stack.push(str[i]);
         
-      } else if(str[i] === '|' || str[i] === '2' || str[i] === '7' || str[i] === '8') {
-        // stack.push(str[i]); 
-        count++;
-        
+      } else if(str[i] === '|' || str[i] === '7' || str[i] === '8') {
+        count++
+        if (count % 2 === 0) {
+          stack.pop();
+        } else {
+          stack.push(str[i]);
+          
+        }
       } else if (stack[stack.length - 1] === map[str[i]]) {
         stack.pop();
       } else return false
   }
-  console.log(count)
   return (stack.length || count % 2 !== 0) ? false: true;
-  
 }
 
